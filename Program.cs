726fcb366867e86a024dc901669f258671500026
Annvel236 <veldymanova@gmail.com>
2020-12-06 12:04:03 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace arrayMinMax
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Random random = new Random();
+      int[] xArray = new int[50];
+
+      for(int i = 0; i < xArray.Length; i++){
+        int elem = random.Next(1, 101);
+        xArray[i] = elem ;
+      }
+
+      // for(int i = 0; i < xArray.Length; i++){
+      //   Console.Write($"{xArray[i]} ");
+      // }
+      // Console.WriteLine("\n");
+
+      Console.WriteLine($"Max: {xArray.Max()}, Min: {xArray.Min()}");
+      
+    }
+  }
+}
